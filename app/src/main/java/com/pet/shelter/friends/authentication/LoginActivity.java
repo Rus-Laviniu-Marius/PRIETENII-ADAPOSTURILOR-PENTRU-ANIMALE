51c51df39d7413ba56c8743dc199076b5cc27eed
Rus-Laviniu-Marius <ruslaviniumarius@gmail.com>
2022-11-29 15:38:59 +0200

    Add app splash screen

---
+++ app/src/main/java/com/pet/shelter/friends/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.pet.shelter.friends.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pet.shelter.friends.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
